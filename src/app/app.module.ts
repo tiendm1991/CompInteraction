d075e05d761691097b76c1864461f8a95a160225
TienDM <TienDM@TienDM>
2017-07-14 17:12:05 +0700

    parent-child interaction

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeroChildComponent } from './hero-child.component';
+import { HeroParentComponent} from './hero-parent.compenent';
+
+let directives: any[] = [
+  AppComponent,
+  HeroParentComponent,
+  HeroChildComponent
+];
+
+let schemas: any[] = [];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: directives,
   imports: [
     BrowserModule
   ],
   providers: [],
+  schemas : schemas,
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/hero.ts
@@ -0,0 +1,12 @@
+/**
+ * New typescript file
+ */
+export class Hero {
+  name:string;
+}
+
+export const HEROES = [
+  {name : 'songoku'},
+  {name : 'wukong'},
+  {name : 'naruto'}
+]
